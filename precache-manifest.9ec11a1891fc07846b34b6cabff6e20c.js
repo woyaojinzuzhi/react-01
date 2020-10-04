a8c92f79f1e780bdd2a77a392c66a10f813479d4
woyaojinzuzhi <2801097664@qq.com>
2020-10-04 19:46:45 +0800

    zuoye1

---
+++ precache-manifest.9ec11a1891fc07846b34b6cabff6e20c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a9c57aaa431d93039dd7e5e718a109",
+    "url": "/index.html"
+  },
+  {
+    "revision": "217f6969c840dac1247f",
+    "url": "/static/css/main.cf50ac1b.chunk.css"
+  },
+  {
+    "revision": "d947744307ce2949b4b7",
+    "url": "/static/js/2.ab0a283b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab0a283b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217f6969c840dac1247f",
+    "url": "/static/js/main.04abe7b6.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
